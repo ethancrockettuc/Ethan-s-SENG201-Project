42c84e9ebb83660c88f482cfe80fb42acbffac7f
ethancrockettuc <130020422+ethancrockettuc@users.noreply.github.com>
2023-05-18 00:10:55 +1200

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,8 +1,8 @@
 public class Item {
-	public String itemName;
-	public String itemAttribute;
-	public int attributeIncrease;
-	public int itemCost;
+	private String itemName;
+	private String itemAttribute;
+	private int attributeIncrease;
+	private int itemCost;
 	
 	public Item(String attribute, int increase, int cost) {
 		itemAttribute = attribute;
@@ -10,23 +10,20 @@ public class Item {
 		itemCost = cost;
 	}
 	
-	public void useItem(Athlete userAthlete) {
-		int skillBeforeUpgrade = userAthlete.athleteSkill;
-		userAthlete.athleteSkill += attributeIncrease;
-		System.out.println(userAthlete.athleteName + "'s " + itemAttribute + " has been increased by " + attributeIncrease + " from " + skillBeforeUpgrade + " to " + userAthlete.athleteSkill + ".");
+	public String getItemEffects() {
+		return ("The item attribute is " + itemAttribute + " and it's attribute increase is " + attributeIncrease );
+	}
+	public String getItemName() {
+		return itemName;
 	}
 	
-	
-	public Item getItemByName(String userInputItem) {
-	    for (Item userItem : userInventory.itemsOwned) {
-	        if (userItem.itemName.equals(userInputItem)) {
-	            return userItem;
-	        }
-	    }
-	    System.out.println(" No Item with the name: "+ userInputItem " !"); // Only happens if userInputItem not matched with Item name in Inventory.
-		
-		
+	public int getAttributeIncrease() {
+		return attributeIncrease;
 	}
-}	
+	public String getItemAttribute() {
+		return itemAttribute;
+	}
+	
+}
 
 
