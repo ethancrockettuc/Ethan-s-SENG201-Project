765698aa806868488fdc37004f59bb8150142570
ethancrockettuc <130020422+ethancrockettuc@users.noreply.github.com>
2023-05-18 00:10:11 +1200

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -2,14 +2,15 @@ import java.util.ArrayList;
 
 
 public class Inventory {
-	public ArrayList<Item> itemsOwned = new ArrayList<Item>();
-	public int currentMoney;
+	private ArrayList<Item> itemsOwned = new ArrayList<Item>();
+	private int currentMoney;
 	
-	public void checkInventory() {
-		System.out.println("Here are the following items you currently own: " + itemsOwned);
-		System.out.println("Here is your current balance: " + currentMoney);
+	public int getCurrentMoney() {
+		return currentMoney;
 	}
-	
+	public void setCurrentMoney(int inputMoney) {
+			this.currentMoney = inputMoney;
+		}
 	public void addItem(Item marketItem) {
 		itemsOwned.add(marketItem);
 	}
@@ -17,5 +18,32 @@ public class Inventory {
 	public void removeItem(Item marketItem) {
 		itemsOwned.remove(marketItem);
 	}
-
+	public ArrayList<Item> getItemsOwned() {
+		return itemsOwned;
+	}
+	public void displayItemsOwned(ArrayList<Item> itemsOwned) {
+		for(Item userItem : itemsOwned) {
+			System.out.println("The name of the item is " + userItem.getItemName() + "and the item effects are " + userItem.getItemEffects());
+		}
+	}
+	public void useItem(Athlete userAthlete, Item userItem) {
+		int skillBeforeUpgrade = userAthlete.getAthleteSkill();
+		userAthlete.setAthleteSkill(skillBeforeUpgrade + userItem.getAttributeIncrease());
+		System.out.println(userAthlete.getAthleteName() + "'s " + userItem.getItemAttribute() + " has been increased by " + userItem.getAttributeIncrease() + " from " + skillBeforeUpgrade + " to " + userAthlete.getAthleteSkill() + ".");
+	}
+	
+	
+	/*
+	//This method gets the name of item, from inventory. not sure if necessary. 
+	public Item getItemByName(String userInputItem, Inventory userInventory) {
+	    for (Item userItem : userInventory.getItemsOwned()) {
+	        if (userItem.ItemName().equals(userInputItem)) {
+	            return userItem;
+	        }
+	    }
+	    System.out.println("No item with the name: " + userInputItem + " found in the inventory.");
+	    return null; // Return null if item is not found
+	}	
+	*/
+	
 }
